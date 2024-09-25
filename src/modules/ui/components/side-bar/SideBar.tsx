d9baf031addf48f9ceea8558138d4eca93c2c98c
Cristian Martinez <cristiancamilomartinezs@gmail.com>
2024-09-25 10:09:10 -0500

    overflow sidebar see elements

--- src/modules/ui/components/side-bar/SideBar.tsx
+++ src/modules/ui/components/side-bar/SideBar.tsx
@@ -56,7 +56,7 @@ export const Sidebar = () => {
       {/* Sidemenu */}
       <nav
         className={clsx(
-          "fixed p-5 right-0 top-0 w-[350px] md:w-[500px] h-screen bg-white z-20 shadow-2xl transform transition-all duration-300",
+          "fixed p-5 right-0 top-0 w-[350px] md:w-[500px] h-screen bg-white z-20 shadow-2xl transform transition-all duration-300 overflow-auto",
           {
             "translate-x-full": !isSideMenuOpen,
           }
