55ec426c53f1c121311ac7ec7d500d1f9eae00b0
Cristian Martinez <cristiancamilomartinezs@gmail.com>
2024-09-25 10:22:16 -0500

    sidebar mt responsive

--- src/modules/ui/components/side-bar/SideBar.tsx
+++ src/modules/ui/components/side-bar/SideBar.tsx
@@ -125,7 +125,7 @@ export const Sidebar = () => {
             <Link
               href="/profile"
               onClick={() => closeMenu()}
-              className="flex items-center p-2 hover:bg-gray-100 rounded transition-all"
+              className="flex items-center p-2 mt-0 md:mt-10 hover:bg-gray-100 rounded transition-all"
             >
               <IoPersonOutline size={30} />
               <span className="ml-3 text-xl">Perfil</span>
