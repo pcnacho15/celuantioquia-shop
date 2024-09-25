46e4500d3799a8bc63b18c364ed95a65ef97df95
Cristian Martinez <cristiancamilomartinezs@gmail.com>
2024-09-25 09:57:12 -0500

    remove px-10 in checkou and cart page

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -6,7 +6,7 @@ import { OrderSummary } from "./ui/OrderSummary";
 
 export default function CartPage() {
   return (
-    <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
+    <div className="flex justify-center items-center mb-72 px-0">
       <div className="flex flex-col w-[1000px]">
         <Title title="Mi carrito de compras" />
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
--- src/app/(shop)/checkout/(checkout)/page.tsx
+++ src/app/(shop)/checkout/(checkout)/page.tsx
@@ -9,7 +9,7 @@ export default function CheckoutPage() {
 
 
   return (
-    <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
+    <div className="flex justify-center items-center mb-72 px-0">
       <div className="flex flex-col w-[1000px]">
         <Title title="Verificar orden" />
 
